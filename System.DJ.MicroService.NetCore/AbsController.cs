5a81dbf2486e578f446dc4b5f9c718e2c2ce40e2
DJ-PC <DJ-PC@DESKTOP-PACAU5I>
2021-08-09 02:16:49 +0800

    v0216

---
+++ System.DJ.MicroService.NetCore/AbsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.DJ.MicroService
+{
+    public abstract class AbsController: ControllerBase
+    {
+        public override RedirectToRouteResult RedirectToRoute(string routeName, object routeValues, string fragment)
+        {
+            return base.RedirectToRoute(routeName, routeValues, fragment);
+        }
+
+        public override RedirectToActionResult RedirectToAction(string actionName, string controllerName, object routeValues, string fragment)
+        {
+            return base.RedirectToAction(actionName, controllerName, routeValues, fragment);
+        }
+    }
+}
