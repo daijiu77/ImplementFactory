91c238d9380da751f1211101c840a2861605c0cd
DJ-PC <DJ-PC@DESKTOP-PACAU5I>
2021-08-09 13:53:04 +0800

    v232

---
+++ Test.NetCoreApi/Controllers/UserInfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.DJ.MicroService;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.NetCoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserInfoController : AbsController
+    {
+        [HttpGet, Route("GetUserInfo")]        
+        public object GetUserInfo()
+        {
+            return new { name = "ZS", age = 21 };
+        }
+    }
+}
