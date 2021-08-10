b5ab7839aae3ba6f4aa4da983a60e7eb0782e575
DJ-PC <DJ-PC@DESKTOP-PACAU5I>
2021-08-11 07:19:23 +0800

    v1212

--- System.DJ.ImplementFactory.Framework/Commons/AsynicTransaction.cs
+++ System.DJ.ImplementFactory.Framework/Commons/AsynicTransaction.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -11,6 +13,8 @@ namespace System.DJ.ImplementFactory.Commons
         private static bool isRunning = true;
         private static Dictionary<string, AsynicElement> dic = new Dictionary<string, AsynicElement>();
         private static int sleepNum = 100;
+
+        private static object objThis = null;
 
         static AsynicTransaction()
         {
@@ -37,6 +41,17 @@ namespace System.DJ.ImplementFactory.Commons
             }
         }
 
+        private static void getSourceObj()
+        {
+            StackTrace trace = new StackTrace();
+            StackFrame stackFrame = trace.GetFrame(2);
+            MethodBase methodBase = stackFrame.GetMethod();
+            string methodName = methodBase.Name;
+            object impl = methodBase.DeclaringType;
+            if (impl.GetType() == typeof(AsynicTransaction)) return;
+            objThis = impl;
+        }
+
         /// <summary>
         /// 向计时器添加一个以100毫秒为间隔的执行任务
         /// </summary>
@@ -46,6 +61,7 @@ namespace System.DJ.ImplementFactory.Commons
         /// <param name="action">执行任务</param>
         public static AsynicData Add(string taskName, int milliseconds_100, int whileCount, Action<AsynicData> action)
         {
+            getSourceObj();
             AsynicElement asynicElement = null;
             dic.TryGetValue(taskName, out asynicElement);
             if (null != asynicElement) return asynicElement.asynicData;
@@ -63,6 +79,7 @@ namespace System.DJ.ImplementFactory.Commons
         /// <param name="action">执行任务</param>
         public static AsynicData Add(string taskName, int milliseconds_100, Action<AsynicData> action)
         {
+            getSourceObj();
             int whileCount = 0;
             AsynicData asynicData = Add(taskName, milliseconds_100, whileCount, action);
             return asynicData;
@@ -78,6 +95,7 @@ namespace System.DJ.ImplementFactory.Commons
         /// <returns></returns>
         public static AsynicData AddOf10ms(string taskName, int milliseconds_10, int whileCount, Action<AsynicData> action)
         {
+            getSourceObj();
             AsynicElement asynicElement = null;
             dic.TryGetValue(taskName, out asynicElement);
             if (null != asynicElement) return asynicElement.asynicData;
@@ -95,6 +113,7 @@ namespace System.DJ.ImplementFactory.Commons
         /// <returns></returns>
         public static AsynicData AddOf10ms(string taskName, int milliseconds_10, Action<AsynicData> action)
         {
+            getSourceObj();
             int whileCount = 0;
             return AddOf10ms(taskName, milliseconds_10, whileCount, action);
         }
@@ -153,6 +172,7 @@ namespace System.DJ.ImplementFactory.Commons
                     ae.WhileCount = whileCount;
                     ae.action = action;
                     ae.TaskName = taskName;
+                    ae.objThis = objThis;
                     dic.Add(taskName, ae);
 
                     foreach (var item in dic)
@@ -186,6 +206,9 @@ namespace System.DJ.ImplementFactory.Commons
     public class AsynicElement
     {
         private SynchronizationContext m_SyncContext = SynchronizationContext.Current;
+
+        private delegate void CallControl();
+
         public string TaskName { get; set; }
 
         public Action<AsynicData> action { get; set; }
@@ -207,6 +230,8 @@ namespace System.DJ.ImplementFactory.Commons
 
         public AsynicData asynicData { get; } = new AsynicData();
 
+        public object objThis { get; set; }
+
         public void exec()
         {
             Task task = new Task(() =>
@@ -215,6 +240,31 @@ namespace System.DJ.ImplementFactory.Commons
                 if (null != m_SyncContext)
                 {
                     m_SyncContext.Post(PostFunction, null);
+                }
+                else if (null != objThis)
+                {
+                    MethodInfo method = objThis.GetType().GetMethod("Invoke", BindingFlags.Public | BindingFlags.Instance);
+                    if (null != method)
+                    {
+                        CallControl cc = delegate ()
+                        {
+                            PostFunction(null);
+                        };
+
+                        try
+                        {
+                            method.Invoke(objThis, new object[] { cc });
+                        }
+                        catch (Exception)
+                        {
+                            PostFunction(null);
+                            //throw;
+                        }
+                    }
+                    else
+                    {
+                        PostFunction(null);
+                    }
                 }
                 else
                 {
--- System.DJ.MicroService.NetCore/FilterController.cs
+++ System.DJ.MicroService.NetCore/FilterController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Newtonsoft.Json;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace System.DJ.MicroService.NetCore
@@ -11,7 +12,10 @@ namespace System.DJ.MicroService.NetCore
         private string svrConfigFile = "svr_config.dt";
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            IQueryCollection qc = context.HttpContext.Request.Query;
+            IQueryCollection query = context.HttpContext.Request.Query;
+            Stream stream = context.HttpContext.Request.Body;
+            //IFormCollection form = context.HttpContext.Request.Form;
+            IHeaderDictionary head = context.HttpContext.Request.Headers;
             //context.Result = new JsonResult(new { data="this is a test."});
             //base.OnActionExecuting(context);
         }
