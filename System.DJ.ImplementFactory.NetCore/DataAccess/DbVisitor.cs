16aed0e2465b8d6f6ad40dc9a84b072363dfccd5
DJ-Laptop <DJ-Laptop@DESKTOP-UQOTLEK>
2022-06-26 19:30:29 +0800

    v0626

---
+++ System.DJ.ImplementFactory.NetCore/DataAccess/DbVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DJ.ImplementFactory.DataAccess.FromUnit;
+using System.Text;
+
+namespace System.DJ.ImplementFactory.DataAccess
+{
+    public class DbVisitor
+    {
+        public static void CreateSqlFrom(params AbsFromUnit[] absFromUnits)
+        {
+
+        }
+    }
+}
---
+++ System.DJ.ImplementFactory.NetCore/DataAccess/FromUnit/LeftJoin.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DJ.ImplementFactory.DataAccess.Pipelines;
+using System.Text;
+
+namespace System.DJ.ImplementFactory.DataAccess.FromUnit
+{
+    public class LeftJoin : AbsFromUnit
+    {
+        public void From(IDataModel dataModel)
+        {
+            //
+        }
+
+        public void From<T>(T dataModel, Func<T, bool> condition) where T : IDataModel
+        {
+            //
+        }
+
+        public void From(IDataModel dataModel, string alias)
+        {
+            //
+        }
+
+        public void From(IDataModel dataModel, string alias, string condition)
+        {
+            //
+        }
+
+        public void From<T>(T dataModel, string alias, Func<T, bool> condition) where T : IDataModel
+        {
+            //
+        }
+    }
+}
