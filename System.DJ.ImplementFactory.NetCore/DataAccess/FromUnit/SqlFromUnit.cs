745b2663b1319e27b5f0c27dbe48f3c9e0b64f86
DJ-Laptop <DJ-Laptop@DESKTOP-UQOTLEK>
2022-09-15 18:30:51 +0800

    ORM：SqlFromUnit.From可根据对象类型调用该方法

--- System.DJ.ImplementFactory.NetCore/DataAccess/FromUnit/SqlFromUnit.cs
+++ System.DJ.ImplementFactory.NetCore/DataAccess/FromUnit/SqlFromUnit.cs
@@ -6,6 +6,8 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
 {
     public class SqlFromUnit
     {
+        private string objErr = "'dataModel' must be a subclass of AbsDataModel.";
+        private string typeErr = "Type 'modelType' must be a subclass of AbsDataModel.";
         protected SqlFromUnit() { }
 
         public static SqlFromUnit New { get { return new SqlFromUnit(); } }
@@ -21,9 +23,30 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
             return this;
         }
 
+        public SqlFromUnit From(object dataModel)
+        {
+            if(null == (dataModel as AbsDataModel))
+            {
+                throw new Exception(objErr);
+            }
+            this.dataModel = (AbsDataModel)dataModel;
+            modelType = dataModel.GetType();
+            return this;
+        }
+
         public SqlFromUnit From<T>() where T : AbsDataModel
         {
             modelType = typeof(T);
+            return this;
+        }
+
+        public SqlFromUnit From(Type modelType)
+        {
+            if (!typeof(AbsDataModel).IsAssignableFrom(modelType))
+            {
+                throw new Exception(typeErr);
+            }
+            this.modelType = modelType;
             return this;
         }
 
@@ -35,10 +58,33 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
             return this;
         }
 
+        public SqlFromUnit From(object dataModel, string alias)
+        {
+            if (null == (dataModel as AbsDataModel))
+            {
+                throw new Exception(objErr);
+            }
+            this.dataModel = (AbsDataModel)dataModel;
+            modelType = dataModel.GetType();
+            this.alias = alias;
+            return this;
+        }
+
         public SqlFromUnit From<T>(string alias) where T : AbsDataModel
         {
             this.alias = alias;
             modelType = typeof(T);
+            return this;
+        }
+
+        public SqlFromUnit From(Type modelType, string alias)
+        {
+            if (!typeof(AbsDataModel).IsAssignableFrom(modelType))
+            {
+                throw new Exception(typeErr);
+            }
+            this.modelType = modelType;
+            this.alias = alias;
             return this;
         }
 
@@ -51,11 +97,36 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
             return this;
         }
 
+        public SqlFromUnit From(object dataModel, string alias, params ConditionItem[] conditions)
+        {
+            if (null == (dataModel as AbsDataModel))
+            {
+                throw new Exception(objErr);
+            }
+            this.dataModel = (AbsDataModel)dataModel;
+            this.alias = alias;
+            this.conditions = conditions;
+            modelType = dataModel.GetType();
+            return this;
+        }
+
         public SqlFromUnit From<T>(string alias, params ConditionItem[] conditions) where T : AbsDataModel
         {
             this.alias = alias;
             this.conditions = conditions;
             modelType = typeof(T);
+            return this;
+        }
+
+        public SqlFromUnit From(Type modelType, string alias, params ConditionItem[] conditions)
+        {
+            if (!typeof(AbsDataModel).IsAssignableFrom(modelType))
+            {
+                throw new Exception(typeErr);
+            }
+            this.alias = alias;
+            this.conditions = conditions;
+            this.modelType = modelType;
             return this;
         }
 
@@ -72,6 +143,23 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
             return this;
         }
 
+        public SqlFromUnit From(object dataModel, string alias, Func<object, bool> funcCondition, params ConditionItem[] conditions)
+        {
+            if (null == (dataModel as AbsDataModel))
+            {
+                throw new Exception(objErr);
+            }
+            this.dataModel = (AbsDataModel)dataModel;
+            this.alias = alias;
+            this.conditions = conditions;
+            modelType = dataModel.GetType();
+            if (null != funcCondition)
+            {
+                this.funcCondition = dm => { return funcCondition(dm); };
+            }
+            return this;
+        }
+
         public SqlFromUnit From<T>(string alias, Func<T, bool> funcCondition, params ConditionItem[] conditions) where T : AbsDataModel
         {
             this.alias = alias;
@@ -80,6 +168,22 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
             if (null != funcCondition)
             {
                 this.funcCondition = dm => { return funcCondition((T)dm); };
+            }
+            return this;
+        }
+
+        public SqlFromUnit From(Type modelType, string alias, Func<object, bool> funcCondition, params ConditionItem[] conditions)
+        {
+            if (!typeof(AbsDataModel).IsAssignableFrom(modelType))
+            {
+                throw new Exception(typeErr);
+            }
+            this.alias = alias;
+            this.conditions = conditions;
+            this.modelType = modelType;
+            if (null != funcCondition)
+            {
+                this.funcCondition = dm => { return funcCondition(dm); };
             }
             return this;
         }
@@ -95,12 +199,41 @@ namespace System.DJ.ImplementFactory.DataAccess.FromUnit
             return this;
         }
 
+        public SqlFromUnit From(object dataModel, Func<object, bool> funcCondition)
+        {
+            if (null == (dataModel as AbsDataModel))
+            {
+                throw new Exception(objErr);
+            }
+            this.dataModel = (AbsDataModel)dataModel;
+            modelType = dataModel.GetType();
+            if (null != funcCondition)
+            {
+                this.funcCondition = dm => { return funcCondition(dm); };
+            }
+            return this;
+        }
+
         public SqlFromUnit From<T>(Func<T, bool> funcCondition) where T : AbsDataModel
         {
             modelType = typeof(T);
             if (null != funcCondition)
             {
                 this.funcCondition = dm => { return funcCondition((T)dm); };
+            }
+            return this;
+        }
+
+        public SqlFromUnit From(Type modelType, Func<object, bool> funcCondition)
+        {
+            if (!typeof(AbsDataModel).IsAssignableFrom(modelType))
+            {
+                throw new Exception(typeErr);
+            }
+            this.modelType = modelType;
+            if (null != funcCondition)
+            {
+                this.funcCondition = dm => { return funcCondition(dm); };
             }
             return this;
         }
