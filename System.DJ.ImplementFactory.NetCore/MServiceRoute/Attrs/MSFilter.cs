79916522c20905ddb1ac6ca9288d1ab8a5c8926f
Dai Jiu <564343162@qq.com>
2023-04-05 13:21:02 +0800

    v19 修正注册客户端IP地址和设置注册有效性无需过滤ip

--- System.DJ.ImplementFactory.NetCore/MServiceRoute/Attrs/MSFilter.cs
+++ System.DJ.ImplementFactory.NetCore/MServiceRoute/Attrs/MSFilter.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Reflection;
 
 namespace System.DJ.ImplementFactory.MServiceRoute.Attrs
 {
@@ -12,12 +13,26 @@ namespace System.DJ.ImplementFactory.MServiceRoute.Attrs
 
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            Type type = context.ActionDescriptor.GetType();
-            string ip = GetIP(context);
-            if (!_kvDic.ContainsKey(ip))
+            bool mbool = false;
+            Type type = context.Controller.GetType();
+            string actionName = context.ActionDescriptor.DisplayName;
+            MethodInfo mi = type.GetMethod(actionName);
+            if (null != mi)
             {
-                throw new Exception("Illegal access");
+                Attribute atr1 = mi.GetCustomAttribute(typeof(MSClientRegisterAction), true);
+                Attribute atr2 = mi.GetCustomAttribute(typeof(MSConfiguratorAction), true);
+                if (null != atr1 || null != atr2) mbool = true;
             }
+
+            if(!mbool)
+            {
+                string ip = GetIP(context);
+                if (!_kvDic.ContainsKey(ip))
+                {
+                    throw new Exception("Illegal access");
+                }
+            }
+            
             base.OnActionExecuting(context);
         }
 
