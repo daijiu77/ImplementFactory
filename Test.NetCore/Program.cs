e938f08512d66ad3afbc6a5a045bf0bd9856536a
QHTZ-PC <QHTZ-PC@DESKTOP-A89PNDR>
2023-06-27 21:06:12 +0800

    v20230627.2106

--- Test.NetCore/Program.cs
+++ Test.NetCore/Program.cs
@@ -475,6 +475,9 @@ namespace Test.NetCore
 
             public void test_user()
             {
+                Type typeList = typeof(DOList<>);
+                typeList = typeList.MakeGenericType(typeof(UserInfo));
+                string ty = typeList.TypeToString(true);
                 string s = "select * from (select top 1 * from UserInfo where name like '%abc%') tb order by age";
                 Regex rg1 = new Regex(@"\sorder\s+by\s+((((?!\()(?!\))(?!\sfrom\s)(?!\swhere\s)(?!\sand\s)(?!\sor\s)(?!\slike\s)).)+)$", RegexOptions.IgnoreCase);
                 if (rg1.IsMatch(s))
